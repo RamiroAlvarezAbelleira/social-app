82e05bc134dea9f28e0064a0fdc4a09b2c0f426c
Ramiro Alvarez Abelleira <alvarezramiro1997@gmail.com>
2025-03-14 18:10:41 +0100

    added flatList

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,14 +2,17 @@ import PostCard from "@/components/posts/PostCard";
 import { ThemedText } from "@/components/ui/ThemedText";
 import { ThemedView } from "@/components/ui/ThemedView";
 import posts from "@/mocks/posts";
+import { FlatList } from "react-native";
 
 export default function HomeScreen() {
   return (
     <ThemedView mainContainer>
-      <ThemedText>Home</ThemedText>
-      {posts.map((post) => (
-        <PostCard key={post.id} {...post} />
-      ))}
+      <FlatList
+        data={posts}
+        renderItem={(post) => (
+          <PostCard {...post.item} />
+        )}
+      />
     </ThemedView>
   );
 }
