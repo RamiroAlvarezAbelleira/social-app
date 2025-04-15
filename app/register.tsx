9f7acc561a982aae964dc892866a5a1b5c59194d
Ramiro Alvarez Abelleira <alvarezramiro1997@gmail.com>
2025-04-15 16:41:34 +0200

    register working, needs some adjusting

--- app/register.tsx
+++ app/register.tsx
@@ -1,17 +1,27 @@
 import { ThemedView } from "@/components/ui/ThemedView";
 import { auth } from "@/lib/firebase";
+import { registerUser } from "@/services/users";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react";
 import { Button, TextInput } from "react-native";
 
 export default function RegisterScreen() {
   const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [profilePicUrl, setProfilePicUrl] = useState('');
   const [password, setPassword] = useState('');
 
   const register = async () => {
     try {
-      await createUserWithEmailAndPassword(auth, email, password);
-      console.log("User registered");
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user
+
+      const idToken = await user.getIdToken()
+
+      const response = await registerUser({ email, username, firstName, lastName, profilePicUrl, idToken })
+      console.log("User registered", response.data);
     } catch (err) {
       console.error(err);
     }
@@ -20,6 +30,10 @@ export default function RegisterScreen() {
   return (
     <ThemedView mainContainer>
       <TextInput placeholder="Email" onChangeText={setEmail} value={email} />
+      <TextInput placeholder="username" onChangeText={setUsername} value={username} />
+      <TextInput placeholder="First Name" onChangeText={setFirstName} value={firstName} />
+      <TextInput placeholder="Last Name" onChangeText={setLastName} value={lastName} />
+      <TextInput placeholder="Profile Pic Url" onChangeText={setProfilePicUrl} value={profilePicUrl} />
       <TextInput placeholder="Password" secureTextEntry onChangeText={setPassword} value={password} />
       <Button title="Register" onPress={register} />
     </ThemedView>
--- services/users.ts
+++ services/users.ts
@@ -9,3 +9,21 @@ export const fetchUserById = async (id: string) => {
     const response = await axios.get(`/users/${id}`)
     return response.data
 }
+
+interface registerParams {
+    email: string,
+    username: string,
+    firstName: string,
+    lastName: string,
+    profilePicUrl: string,
+    idToken: string
+}
+
+export const registerUser = async ({ email, username, firstName, lastName, profilePicUrl, idToken }: registerParams) => {
+    const response = await axios.post("/users/register", { email, username, firstName, lastName, profilePicUrl }, {
+        headers: {
+            Authorization: `Bearer ${idToken}`
+        }
+    })
+    return response.data
+}
