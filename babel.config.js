c10d86f87ea6e364d50521b44826efb93a16a692
Ramiro Alvarez Abelleira <alvarezramiro1997@gmail.com>
2025-04-16 16:13:21 +0200

    fixed dotenv bug

--- babel.config.js
+++ babel.config.js
@@ -4,12 +4,6 @@ module.exports = function (api) {
         presets: [
             ["babel-preset-expo", { jsxImportSource: "nativewind" }],
             "nativewind/babel",
-        ],
-        plugins: [
-            ['module:react-native-dotenv', {
-                moduleName: '@env',
-                path: '.env',
-            }]
-        ],
+        ]
     };
 };
