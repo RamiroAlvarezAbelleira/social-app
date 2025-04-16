a707a94a5a410cf6f821706e6d186319b22d26a2
Ramiro Alvarez Abelleira <alvarezramiro1997@gmail.com>
2025-04-16 21:31:55 +0200

    wip login and register with react-hook-form and zod

--- app/login.tsx
+++ app/login.tsx
@@ -3,11 +3,33 @@ import { auth } from "@/lib/firebase";
 import { useState } from "react";
 import { View, TextInput, Button, Text } from "react-native";
 import { ThemedView } from "@/components/ui/ThemedView";
+import { ScrollView } from "react-native";
+import { ThemedText } from "@/components/ui/ThemedText";
+import CustomInput from "@/components/ui/CustomInput";
+import { useRouter } from "expo-router";
+import PressableView from "@/components/ui/PressableView";
+import { useTheme } from "@react-navigation/native";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { loginSchema } from "@/schemas/loginSchema";
+import CustomButton from "@/components/ui/CustomButton";
+
+type LoginFormData = {
+  email: string;
+  password: string
+}
 
 export default function LoginScreen() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const login = async () => {
+  const router = useRouter()
+  const { colors } = useTheme()
+
+  const { control, handleSubmit, formState: { errors } } = useForm({
+    resolver: zodResolver(loginSchema)
+  })
+
+
+  const login = async (data: LoginFormData) => {
+    const { email, password } = data
     try {
       await signInWithEmailAndPassword(auth, email, password);
       console.log("Logged in");
@@ -15,12 +37,57 @@ export default function LoginScreen() {
       console.error(err);
     }
   };
+  
+  const test = async (data: LoginFormData) => {
+    console.log(data)
+  };
+
+  const goToRegister = () => {
+    router.push('/register')
+  }
 
   return (
-    <ThemedView mainContainer>
-      <TextInput placeholder="Email" onChangeText={setEmail} value={email} />
-      <TextInput placeholder="Password" secureTextEntry onChangeText={setPassword} value={password} />
-      <Button title="Login" onPress={login} />
+    <ThemedView mainContainer className="min-h-[100vh]">
+      <ThemedView className="items-center h-[100] justify-center">
+        <ThemedText>LOGO</ThemedText>
+      </ThemedView>
+      <CustomInput
+        name="email"
+        placeholder="email@email.com"
+        control={control}
+        error={errors.email?.message}
+      />
+      <CustomInput
+        name="password"
+        placeholder="Enter password"
+        control={control}
+        error={errors.password?.message}
+        secureTextEntry
+      />
+      <ThemedView className="flex-row justify-center mx-4 gap-x-2">
+        <ThemedText>
+          Don't have an account?
+        </ThemedText>
+        <PressableView onPressFunc={() => { goToRegister() }}>
+          <ThemedText
+            lightColor={colors.primary}
+            darkColor={colors.primary}
+          >Register</ThemedText>
+        </PressableView>
+      </ThemedView>
+
+      <ThemedView className="items-center px-4 absolute bottom-16 w-full">
+        <CustomButton onPressFunc={handleSubmit(test)}>
+          <ThemedText
+            className="font-semibold text-center"
+            lightColor="#ECEDEE"
+            darkColor="#11181C"
+          >
+            Login
+          </ThemedText>
+        </CustomButton>
+      </ThemedView>
+
     </ThemedView>
   );
 }
--- app/register.tsx
+++ app/register.tsx
@@ -1,12 +1,18 @@
+import CustomButton from "@/components/ui/CustomButton";
 import CustomInput from "@/components/ui/CustomInput";
+import PressableView from "@/components/ui/PressableView";
+import { ThemedText } from "@/components/ui/ThemedText";
 import { ThemedView } from "@/components/ui/ThemedView";
 import { auth } from "@/lib/firebase";
 import { registerSchema } from "@/schemas/registerSchema";
 import { registerUser } from "@/services/users";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useTheme } from "@react-navigation/native";
+import { useRouter } from "expo-router";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { useForm } from "react-hook-form";
 import { Button } from "react-native";
+import { ScrollView } from "react-native";
 
 type RegisterFormData = {
   email: string;
@@ -23,6 +29,10 @@ export default function RegisterScreen() {
     resolver: zodResolver(registerSchema)
   })
 
+  const router = useRouter();
+
+  const { colors } = useTheme()
+
   const register = async (data: RegisterFormData) => {
     const { email, username, firstName, lastName, profilePicUrl, password } = data
     try {
@@ -32,7 +42,7 @@ export default function RegisterScreen() {
       const idToken = await user.getIdToken()
 
       const response = await registerUser({ email, username, firstName, lastName, profilePicUrl, idToken })
-      console.log("User registered", response.data);
+      console.log("User registered", response);
     } catch (err) {
       console.error(err);
     }
@@ -42,46 +52,78 @@ export default function RegisterScreen() {
     console.log(data)
   }
 
+  const goToLogin = () => {
+    router.push('/login')
+  }
+
   return (
-    <ThemedView mainContainer>
-      <CustomInput
-        name="email"
-        placeholder="email@email.com"
-        control={control}
-        error={errors.email?.message}
-      />
-      <CustomInput
-        name="username"
-        placeholder="Enter username"
-        control={control}
-        error={errors.username?.message}
-      />
-      <CustomInput
-        name="firstName"
-        placeholder="Enter firstname"
-        control={control}
-        error={errors.firstName?.message}
-      />
-      <CustomInput
-        name="lastName"
-        placeholder="Enter lastname"
-        control={control}
-        error={errors.lastName?.message}
-      />
-      <CustomInput
-        name="profilePicUrl"
-        placeholder="Enter profile photo url"
-        control={control}
-        error={errors.profilePicUrl?.message}
-      />
-      <CustomInput
-        name="password"
-        placeholder="Enter password"
-        control={control}
-        error={errors.password?.message}
-        secureTextEntry
-      />
-      <Button title="Register" onPress={handleSubmit(test)} />
+    <ThemedView mainContainer className="min-h-[100vh]">
+      <ScrollView>
+        <ThemedView className="items-center h-[100] justify-center">
+          <ThemedText>LOGO</ThemedText>
+        </ThemedView>
+        <CustomInput
+          name="email"
+          placeholder="email@email.com"
+          control={control}
+          error={errors.email?.message}
+        />
+        <CustomInput
+          name="username"
+          placeholder="Enter username"
+          control={control}
+          error={errors.username?.message}
+        />
+        <CustomInput
+          name="firstName"
+          placeholder="Enter firstname"
+          control={control}
+          error={errors.firstName?.message}
+        />
+        <CustomInput
+          name="lastName"
+          placeholder="Enter lastname"
+          control={control}
+          error={errors.lastName?.message}
+        />
+        <CustomInput
+          name="profilePicUrl"
+          placeholder="Enter profile photo url"
+          control={control}
+          error={errors.profilePicUrl?.message}
+        />
+        <CustomInput
+          name="password"
+          placeholder="Enter password"
+          control={control}
+          error={errors.password?.message}
+          secureTextEntry
+        />
+        <ThemedView className="pb-[150px] flex-row justify-center mx-4 gap-x-2">
+          <ThemedText>
+            Already have an account?
+          </ThemedText>
+          <PressableView onPressFunc={() => { goToLogin() }}>
+            <ThemedText
+              lightColor={colors.primary}
+              darkColor={colors.primary}
+            >login</ThemedText>
+          </PressableView>
+        </ThemedView>
+      </ScrollView>
+
+      <ThemedView className="items-center px-4 absolute bottom-16 w-full">
+        <CustomButton onPressFunc={handleSubmit(test)}>
+          <ThemedText
+            className="font-semibold text-center"
+            lightColor="#ECEDEE"
+            darkColor="#11181C"
+          >
+            Register
+          </ThemedText>
+        </CustomButton>
+      </ThemedView>
+
     </ThemedView>
   );
 }
